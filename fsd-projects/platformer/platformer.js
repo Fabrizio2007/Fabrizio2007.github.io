c6c300c21c79af681ca990f408966fa1b434cece
Fabrizio2007 <Fabrizio.centeno@jpschools.org>
2025-02-18 21:22:31 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "purple"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -29,10 +29,10 @@ $(function () {
     // TODO 1 - Enable the Grid
      toggleGrid();
     createPlatform(300, 10, 250, 620, "green");
-    createPlatform(700, 700, 50, 20, "blue"); 
-    createPlatform(700, 700, 100, 100, "red");  
-    createPlatform(800, 600, 300, 30, "orange");
-    createPlatform(1000, 500, 300, 20, "purple");
+    createPlatform(700, 700, 50, 20, "purple"); 
+    createPlatform(700, 700, 100, 100, "green");  
+    createPlatform(800, 600, 300, 30, "purple");
+    createPlatform(1000, 500, 300, 20, "green");
   
     // TODO 3 - Create Collectables
     createCollectable("diamond", 500, 860); 
@@ -41,10 +41,10 @@ $(function () {
     
     // TODO 4 - Create Cannons
     createCannon("left", 500, 1200);
-    createCannon("right", 600, 1500);
+    createCannon("right", 650, 1500);
     createCannon("top", 700, 1000);
     createCannon("left", 800, 1300);
-    createCannon("right", 1000, 1600);
+    createCannon("right", 400, 1500);
     createCannon("top", 1000, 1400);
     
     
